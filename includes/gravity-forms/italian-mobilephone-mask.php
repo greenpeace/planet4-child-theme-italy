0acdf2694ae9af1b58c7a3e6865702a3e2d9bfb1
alessiodral <ale5pam80@gmail.com>
2024-06-05 12:26:41 +0200

    Update italian-mobilephone-mask.php

--- includes/gravity-forms/italian-mobilephone-mask.php
+++ includes/gravity-forms/italian-mobilephone-mask.php
@@ -8,7 +8,7 @@
      $phone_formats['br'] = array(
          'label' => 'Numero cellulare italiano',
          // 'mask' => '(+39)999-999-9999',
-         'mask' => false,
+         'mask' => '999.9999999',
          'regex' => '/^(32[^15]|33[^20]|34[0-9]|35[^2-46-9]|36[^45790]|37[^6]|38[^125-7]|39[^4-689])(\d){5,7}/g',
          'error_message_setting' => 'Numero cellulare non valido'
         );
