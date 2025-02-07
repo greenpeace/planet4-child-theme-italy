ffed657031df7b47c035f1058f1303fcfa4c4943
alessiodral <ale5pam80@gmail.com>
2025-02-07 12:08:32 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -22,30 +22,3 @@ function enqueue_child_styles() {
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
 }
 
-
-// function avatar_default($avatar_defaults) {
-//     $nuovo_avatar = get_stylesheet_directory_uri() . '/img/user-avatar-default.png';
-//     $avatar_defaults[$nuovo_avatar] = "Avatar Greenpeace";
-//     return $avatar_defaults;
-// }
-// add_filter('avatar_defaults', 'avatar_default');
-
-
-
-function override_get_avatar_html( $avatar, $id_or_email, $args ) {
-    // URL completo del tuo avatar personalizzato
-    $custom_avatar_url = get_stylesheet_directory_uri() . '/img/user-avatar-default.png';
-    
-    // Verifica se l'HTML contiene l'URL del default avatar del tema (in questo esempio "p4-avatar.jpg")
-    if ( strpos( $avatar, 'p4-avatar.jpg' ) !== false ) {
-        // Sostituisci l'URL del default con il tuo URL personalizzato
-        $avatar = str_replace(
-            get_template_directory_uri() . '/images/p4-avatar.jpg',
-            $custom_avatar_url,
-            $avatar
-        );
-    }
-    return $avatar;
-}
-add_filter( 'get_avatar', 'override_get_avatar_html', 10, 3 );
-
