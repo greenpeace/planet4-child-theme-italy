8f9177b94a9bca5218dc05302d7523c42e3e809a
alessiodral <ale5pam80@gmail.com>
2025-02-07 12:06:44 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -32,15 +32,20 @@ function enqueue_child_styles() {
 
 
 
-function override_default_avatar( $args, $id_or_email ) {
-    // Qui controlli se il parametro 'default' è uguale a quello impostato dal tema
-    $tema_default = get_template_directory_uri() . '/images/p4-avatar.jpg';
-
-    if ( isset( $args['default'] ) && $args['default'] === $tema_default ) {
-        // Sostituisci con il tuo avatar personalizzato
-        $args['default'] = get_stylesheet_directory_uri() . '/img/user-avatar-default.png';
-    }
+function override_get_avatar_html( $avatar, $id_or_email, $args ) {
+    // URL completo del tuo avatar personalizzato
+    $custom_avatar_url = get_stylesheet_directory_uri() . '/img/user-avatar-default.png';
     
-    return $args;
+    // Verifica se l'HTML contiene l'URL del default avatar del tema (in questo esempio "p4-avatar.jpg")
+    if ( strpos( $avatar, 'p4-avatar.jpg' ) !== false ) {
+        // Sostituisci l'URL del default con il tuo URL personalizzato
+        $avatar = str_replace(
+            get_template_directory_uri() . '/images/p4-avatar.jpg',
+            $custom_avatar_url,
+            $avatar
+        );
+    }
+    return $avatar;
 }
-add_filter( 'pre_get_avatar_data', 'override_default_avatar', 10, 2 );
+add_filter( 'get_avatar', 'override_get_avatar_html', 10, 3 );
+
